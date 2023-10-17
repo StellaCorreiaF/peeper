21678d2d1e415fb6b176e7550f1ec40f2247987d
Stella Oliveira <stella.oliveira@locaweb.com.br>
2023-10-17 19:51:29 -0300

    cria estrutura mvc para posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_17_185345) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_17_223035) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string "titulo", null: false
     t.datetime "created_at", null: false
