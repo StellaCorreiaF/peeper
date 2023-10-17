5f1387f21188c2fd521e396b6837065ecd511329
Stella Oliveira <stella.oliveira@locaweb.com.br>
2023-10-17 16:37:25 -0300

    constroi estrutura de view e controller para topicos

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,63 @@
+class TopicsController < ApplicationController
+	before_action :set_topico, only: %i[ show edit update destroy ]
+	def index
+		@topics = Topic.all
+	end
+
+	def show
+		set_topic
+	end
+	 
+	def new 
+		@topic = Topic.new
+	end
+
+	def edit
+		set_topic
+	end
+
+	def create
+    # topico_params = params.require(:topico).permit(:titulo)  - substituimos pelo metodo params
+    @topico = Topic.new(topic_params)
+    
+    respond_to do |format|
+      if @topic.save
+        format.html { redirect_to topico_url(@topic), notice: "Topico was successfully created." }
+        
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        
+      end
+    end
+  end
+
+  def update
+    set_topic
+    # topico_params = params.require(:topico).permit(:titulo)  - substituimos pelo metodo params sendo usado como parametro no if
+
+    if @topic.update(topic_params)
+      # O flash permite empurrar alguns primitivos do ruby até a proxima action q será executada(string, array e hash..)
+      # e aí posso guardar dentro uma msg de sucesso pro usuario. Depois usamos lá na view
+
+      redirect_to topico_url(@topic), notice:"Tópico atualizado!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    redirect_to topics_url, notice: "Tópico Removido"
+  end
+
+
+	private 
+
+	def set_topic
+		@topic = Topic.find(params[:id])
+	end
+
+	def topic_params 
+		params.require(:topic).permit(:titulo)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   get "/topics", to: "topics#index"
+  post "/topics", to: "topics#create"
+  get "/topics/new", to: "topics#new", as: :new_topic
+  get "/topics/:id/edit", to: "topics#edit", as: :edit_topic
+  get "/topics/:id", to: "topics#show", as: :topic
+  patch "topicos/:id", to: "topics#update"
+  put "topics/:id", to: "topics#update"
+  delete "topics/:id", to: "topics#destroy"
 end
