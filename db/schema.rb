fa5aa92c56878d47725a3b27e8570c64ada2a4fb
Stella Oliveira <stella.oliveira@locaweb.com.br>
2023-10-20 16:14:48 -0300

    altera coluna user_id para não poder ser nula

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_204152) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_20_185146) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_204152) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
