523d8ad77ae0a41ae24401f355e6652eaec9b66d
Stella Oliveira <stella.oliveira@locaweb.com.br>
2023-10-25 14:02:20 -0300

    ajusta rotas

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }  
 
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_out' => 'devise/sessions#destroy'    
   end
 
   resources :users, only: [:show, :index] 
