214cb6eaf6a50a42ece6cbfac95b488aac49742b
Stella Oliveira <stella.oliveira@locaweb.com.br>
2023-10-25 16:19:15 -0300

    adiciona validação de exibição de topicos para apenas os que ja possuem posts vinculados

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,7 @@ class WelcomeController < ApplicationController
 		@posts = Post.order(created_at: :desc).all
 		@topics = Topic.all
 		@random_users = User.all.sample(5) 
+		@topics_with_posts = Topic.joins(:posts).distinct
+
 	end
 end
