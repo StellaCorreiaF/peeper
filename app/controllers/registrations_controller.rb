e6ab6a389f5684aabb2928f48a9f8736dccfdc31
Stella Oliveira <stella.oliveira@locaweb.com.br>
2023-10-25 14:35:51 -0300

    corrige erros do devise

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
-class RegistrationsController < Devise::RegistrationsController  
+class RegistrationsController < Devise::RegistrationsController 
+  before_action :configure_account_update_params, only: [:update]
+ 
   private
 
   def sign_up_params
@@ -8,5 +10,10 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :profile_image)
   end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_image])
+  end
+
 
 end
