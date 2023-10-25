eb37c5ccac082df627d1796d25125a293304ea1d
Stella Oliveira <stella.oliveira@locaweb.com.br>
2023-10-25 11:15:01 -0300

    cria lógica para associar topicos e posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,24 +4,30 @@ class Post < ApplicationRecord
   belongs_to :topic, optional: true
 
   after_create :associate_with_topics
-  
+
   def associate_with_topics
-    words = body.downcase.split(/\W+/)  # Coleta todas as palavras do post
+    # Pego todas as palavras do post em minúsculas para remover os acentos
+    words = remove_accents(body.downcase).split(/\W+/)
 
     words.each do |word|
-      # Verifica se a palavra corresponde a um tópico existente
-      topic = Topic.find_by(titulo: word)
+      # Normalizo a palavra e verifico se corresponde a algum tópico existente, ignorando maiusculas ou minusculas
+      normalized_word = normalize_word(word)
+      topic = Topic.where('lower(titulo) = ?', normalized_word).first
 
       if topic
-        # Associa o post ao tópico e para a busca após a primeira correspondência
+        # Associa o post ao tópico e continua a busca
         self.topic = topic
         save
-        break
       end
     end
-  end  
+  end
+
+  def remove_accents(text)
+    text.gsub(/[áàâãéèêíìîóòôõúùûç]/, 'a' => 'a', 'à' => 'a', 'â' => 'a', 'ã' => 'a', 'é' => 'e', 'è' => 'e', 'ê' => 'e', 'í' => 'i', 'ì' => 'i', 'î' => 'i', 'ó' => 'o', 'ò' => 'o', 'ô' => 'o', 'õ' => 'o', 'ú' => 'u', 'ù' => 'u', 'û' => 'u', 'ç' => 'c')
+  end
+
+  def normalize_word(word)
+    # Remove acentos e caracteres especiais
+    word.gsub(/[^0-9a-zA-Z]/, '')
+  end
 end
-
-
-
-  
