b07bba9a5ab93ceab0b82391bb0d4290c66a39be
Stella Oliveira <stella.oliveira@locaweb.com.br>
2023-10-25 11:17:49 -0300

    cria script para popular o banco

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Crie usuários
+User.create(name: 'stella', email: 'tes@teste.com', password: '123456')
+User.create(name: 'joao', email: 'teste2@teste.com', password: '123456')
+	
+
+# Crie posts
+user1 = User.find_by(id: 1)
+user2 = User.find_by(id: 2)
+
+5.times do
+  post = user1.posts.create(    
+    body: 'Que Calor!!!!'
+  )
+  
+  3.times do
+    post.comments.create(
+      body: 'Comentário no Post',
+      user: user2
+    )
+  end
+end
+
+5.times do
+  post = user2.posts.create(
+    body: 'Que frio'
+  )
+  
+  2.times do
+    post.comments.create(
+      body: 'Outro Comentário no Post',
+      user: user1
+    )
+  end
+end
