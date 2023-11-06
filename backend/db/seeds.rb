c8fb2605079724b1b41720af97809aa09a9fdd89
Stella Oliveira <stella.oliveira@locaweb.com.br>
2023-11-06 15:28:46 -0300

    adiciona mais usuarios ao banco

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -16,15 +16,16 @@ end
 # Crie usuários
 user1 = User.create(name: 'stella', email: 'tes@teste.com', password: '123456', password_confirmation: '123456')
 user2 = User.create(name: 'joao', email: 'teste2@teste.com', password: '123456', password_confirmation: '123456')
-
-
+# Crie usuários
+user3 = User.create(name: 'carlinha', email: 'testando@teste.com', password: '123456', password_confirmation: '123456')
+user4 = User.create(name: 'jorginho', email: 'testando2@teste.com', password: '123456', password_confirmation: '123456')
 
 
 # Crie posts
 user1 = User.find_by(id: 1)
 user2 = User.find_by(id: 2)
 
-5.times do
+10.times do
   post = user1.posts.create(    
     body: 'Que Calor!!!!'
   )
@@ -37,7 +38,7 @@ user2 = User.find_by(id: 2)
   end
 end
 
-5.times do
+10.times do
   post = user2.posts.create(
     body: 'Que frio'
   )
@@ -46,6 +47,34 @@ end
     post.comments.create(
       body: 'Outro Comentário no Post',
       user: user1
+    )
+  end
+end
+
+
+10.times do
+  post = user3.posts.create(
+    body: 'Hoje viajei a negócios'
+  )
+  
+  2.times do
+    post.comments.create(
+      body: 'Estou aqui só para testar',
+      user: user4
+    )
+  end
+end
+
+
+10.times do
+  post = user4.posts.create(
+    body: 'Gosto demais de viajar.'
+  )
+  
+  2.times do
+    post.comments.create(
+      body: 'Viajar é bom, seja por qualquer motivo',
+      user: user3
     )
   end
 end
